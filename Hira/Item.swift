42523936b7d4111b60060c09eafd9aa3685ec226
Achmad Andy Dekanovy <achmdndy@gmail.com>
2026-04-02 11:14:45 +0700

    Initial Commit

---
+++ Hira/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Hira
+//
+//  Created by Ryuk on 02/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HiraTests/HiraTests.swift
@@ -0,0 +1,16 @@
+//
+//  HiraTests.swift
+//  HiraTests
+//
+//  Created by Ryuk on 02/04/26.
+//
+
+import Testing
+
+struct HiraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
